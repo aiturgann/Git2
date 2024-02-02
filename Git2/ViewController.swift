3497ceb620fce640fe3a8f494b6d8c548af7e49e
aiturann <aiturgannnnnn@gmail.com>
2024-02-02 22:07:04 +0600

    Initial Commit

---
+++ Git2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Git2
+//
+//  Created by Aiturgan Kurmanbekova on 2/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
