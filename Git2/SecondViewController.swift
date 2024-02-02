51d9b81b0a256b66fd341b10c41823a47dd86147
aiturann <aiturgannnnnn@gmail.com>
2024-02-02 22:16:00 +0600

    SecondVC

--- Git2/SecondViewController.swift
+++ Git2/SecondViewController.swift
@@ -5,4 +5,10 @@
 //  Created by Aiturgan Kurmanbekova on 2/2/24.
 //
 
-import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
